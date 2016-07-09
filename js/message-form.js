5e8da9817e6e119d4d549661bbaa0ea8405424e2
Ostap <tank302@gmail.com>
2016-07-09 09:55:14 +0000

    create contact.html

---
+++ js/message-form.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('#submit').click(function(e)  {
+        e.preventDefault();
+        $.ajax( {
+            url: "https://formspree.io/tank302@gmail.com",
+            method: "POST",
+            data: {email: $('#email').val(),
+                    message: $('#message').val()
+        },
+        dataType: "json"
+    }).done(function() {
+        $('#contact').html('<h4>Дякую! Я невдовзі Вам відповім!</h4>')
+    });
+});
+});
+
+
---
+++ js/skype.js
@@ -0,0 +1,7 @@
+Skype.ui({
+"name": "dropdown",
+"element": "SkypeButton_Call_tank302_1",
+"participants": ["tank302"],
+"imageSize": 24
+});
+                    
